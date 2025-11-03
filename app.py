95d36de723417c08b0875942c4ef29d40cb62c9d
Abj182 <abhinn1010@gmail.com>
2025-11-03 20:41:04 +0530

    updated app.py

--- app.py
+++ app.py
@@ -852,4 +852,5 @@ def search_online():
 # Run App (keep at end of file)
 # ============================================
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
